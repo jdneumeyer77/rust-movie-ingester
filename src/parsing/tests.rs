3fa7f21805c97c24916c27eab35fa7cb5923c953
Jordan Neumeyer <jdneumeyer@gmail.com>
2023-06-27 15:53:00 -0600

    a few more test cases.

--- src/parsing/tests.rs
+++ src/parsing/tests.rs
@@ -7,10 +7,14 @@ use super::super::*;
 
   #[test]
   fn json_to_set() {
-    let json = "[{'id': 28, 'name': 'Action'}, {'id': 12, 'name': 'Adventure'}, {'id': 878, 'name': 'Science Fiction'}, {'id': 53, 'name': 'Thriller'}]";
-
     assert_eq!(convert_json_to_set(""), HashSet::new());
+    assert_eq!(convert_json_to_set("[]"), HashSet::new());
+    let invalid_json = r#"[{"a": 2}, {"a": 3}]"#;
+    assert_eq!(convert_json_to_set(invalid_json), HashSet::new());
+    let json_single_quotes = "[{'id': 28, 'name': 'Action'}, {'id': 12, 'name': 'Adventure'}, {'id': 878, 'name': 'Science Fiction'}, {'id': 53, 'name': 'Thriller'}]";
+    let json = json_single_quotes.replace("'", "\"");
     let expected = HashSet::from(["28","12","878", "53"].map(|x| x.to_string()));
-    assert_eq!(convert_json_to_set(json), expected);
+    assert_eq!(convert_json_to_set(&json), expected);
+    assert_eq!(convert_json_to_set(&json_single_quotes), expected);
   }
 }
